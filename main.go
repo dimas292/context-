c5bfc3d769c4cf5ccec5d7cc56a468c505aca869
dimas <dimasixc22@gmail.com>
2024-08-28 11:25:42 +0700

    "panic"

--- main.go
+++ main.go
@@ -1,23 +1,56 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"os"
 )
 
-func main() {
-
-	names := []string{"Nobee", "bee", "go", "Nodejs"}
-
-	for _, name := range names {
-		if name == "go"{
-			os.Exit(1)
-		}
-		cetak(name)
-	}
+type Student struct {
+	Name   string
+	Height int
 }
 
-func cetak(text string){
+func (s *Student) Validate() error {
+
+	if s.Name == "" {
+		return errors.New("nama tidak boleh kosong")
+	}
+
+	if len(s.Name) <= 3 {
+		panic("nama harus lebih besar dari 3")
+	}
+
+	if s.Height == 0 {
+		return errors.New("tinggi tidak boleh kosong")
+	}
+
+	return nil
+
+}
+
+func main() {
+	defer catchError()
+	student := Student{Name: "dim", Height: 18}
+
+	err := student.Validate()
+	if err != nil {
+		cetak(err.Error())
+	} else {
+		cetak("hello  " + student.Name)
+	}
+
+}
+
+
+func cetak(text string) {
 	fmt.Println(text)
+}
+
+func catchError() {
+	err := recover()
+
+	if err != nil {
+		cetak("error nih : " + fmt.Sprint(err))
+	}
 }
 
