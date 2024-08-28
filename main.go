ca79e0911fe3c11007a9e506c0572450b557f7ac
dimas <dimasixc22@gmail.com>
2024-08-28 10:17:01 +0700

    "ctx"

--- main.go
+++ main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
 
-func main(){
-	fmt.Println("name : dimas")
+type Data struct {
+	Method string
+	Value  string
+}
+
+func main() {
+
+	ctx := context.Background()
+
+	dataA := Data{
+		Method: "GET",
+		Value: "Mengambil data dari context A",
+	}
+
+	ctxLevel1_A := context.WithValue(ctx, "data", dataA)
+
+	dataB := Data{
+		Method: "POST",
+		Value: "Menambah data user pada context B",
+	}
+
+	ctxLevel1_B := context.WithValue(ctx, "data", dataB)
+
+	processA(ctxLevel1_A)
+	processB(ctxLevel1_B)
+	
+
+}
+
+func processA(ctx context.Context){
+	var data Data
+
+	fmt.Println("proccess context level 1 A")
+
+	dataCtx := ctx.Value("data")
+
+	dataCtxByte, err := json.Marshal(dataCtx)
+	if err != nil {
+		fmt.Println("eror parsing")
+		return
+	}
+
+	err = json.Unmarshal(dataCtxByte, &data)
+
+	fmt.Println("try proccess with method: ", data.Method)
+	fmt.Println(data.Value)
+	
+}
+func processB(ctx context.Context){
+
+	var data Data
+
+	fmt.Println("proccess context level 1 A")
+
+	dataCtx := ctx.Value("data")
+
+	dataCtxByte, err := json.Marshal(dataCtx)
+	if err != nil {
+		fmt.Println("error parsing")
+		return
+	}
+
+	err = json.Unmarshal(dataCtxByte, &data)
+
+	fmt.Println("try proccess with method: ", data.Method)
+	fmt.Println(data.Value)
+
 }
